91528fe8c351b877bc780a94d6a3f519d8bcc858
Yevhenii <evg.gordi@gmail.com>
2025-03-25 22:58:45 +0100

    Cv fix 2

--- src/consts/common.consts.ts
+++ src/consts/common.consts.ts
@@ -1,4 +1,4 @@
 export const MY_LINKEDIN_URL =
   "https://www.linkedin.com/in/yevhenii-hordiienko/";
 export const MY_GITHUB_URL = "https://github.com/D3AD-E";
-export const MY_CV = "/files/Resume_Yevhenii_Hordiienko.pdf";
+export const MY_CV = "files/Resume_Yevhenii_Hordiienko.pdf";
